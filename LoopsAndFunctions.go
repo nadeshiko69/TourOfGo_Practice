94b3107b5d1dbcb42f821da8ec2dcddcc6cbc133
SakuWindows <sakuTech.info@gmail.com>
2025-10-31 13:18:18 +0900

    Exercise: Loops and Functions

---
+++ LoopsAndFunctions.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func Sqrt(x float64) float64 {
+	z := float64(1)
+	for i := 0; i < 10; i++ {
+		z -= (z*z - x) / (2 * z)
+	}
+	return z
+}
+
+func main() {
+	tmp := float64(12345)
+	fmt.Println(Sqrt(tmp))
+	fmt.Println(math.Sqrt(tmp))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module tour-of-go-practice
+
+go 1.25.3
